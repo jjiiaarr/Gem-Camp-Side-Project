59c9857c28e6b502b8567a33cbe82168673c7fa8
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-04-20 22:40:35 +0800

    remove useless code on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'welcome/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
