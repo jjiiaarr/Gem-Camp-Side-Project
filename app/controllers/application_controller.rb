1b9e47fdd96fb35609db870d23c7c72225edad1c
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-04-22 17:02:29 +0800

    add routes for Admin sign in and check if the user is admin. also add views and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :phone_number])
   end
+
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource) if is_navigational_format?
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admin_users_path
+    else
+      root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-   root 'home#index'
+  root 'home#index'
+
+  namespace :admin do
+    resources :users
+  end
 end
