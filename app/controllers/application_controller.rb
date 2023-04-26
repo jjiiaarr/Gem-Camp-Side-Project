020dd857d57253dc766ab8130d38806060fbf86e
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-04-27 01:12:34 +0800

    tempo comment application controller codes and add notice,alert on layouts application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :phone_number])
-  end
-
-  def after_sign_up_path_for(resource)
-    after_sign_in_path_for(resource) if is_navigational_format?
-  end
-
-  def after_sign_in_path_for(resource)
-    if current_user.admin?
-      admin_users_path
-    else
-      root_path
-    end
-  end
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  #
+  # protected
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :phone_number])
+  # end
+  #
+  # def after_sign_up_path_for(resource)
+  #   after_sign_in_path_for(resource) if is_navigational_format?
+  # end
+  #
+  # def after_sign_in_path_for(resource)
+  #   if current_user.admin?
+  #     admin_users_path
+  #   else
+  #     root_path
+  #   end
+  # end
 end
