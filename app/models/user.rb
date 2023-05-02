5075da468fe9d9652a7d02bc275577cddf09be1a
jjiiaarr <jiarr.glorioso@gmail.com>
2023-05-02 17:32:35 +0800

    [Feature 6] Add has_many association to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,6 @@ class User < ApplicationRecord
 
   belongs_to :parent, class_name: 'User', counter_cache: :children_members, optional: true
   has_many :children, class_name: 'User', foreign_key: :parent_id
+
+  has_many :user_address
 end
