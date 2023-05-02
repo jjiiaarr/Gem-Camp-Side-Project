72f71f67fe230f29b1fd50025e99eb4a5ad9c0a7
jjiiaarr <jiarr.glorioso@gmail.com>
2023-05-02 17:32:02 +0800

    [Feature 6] Add enum and phone number validation to user_address model

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -1,2 +1,10 @@
 class UserAddress < ApplicationRecord
+  enum genre: { home: 0, office: 1 }
+
+  validates :phone_number, phone: {
+    possible: true,
+    allow_blank: true,
+    types: %i[voip mobile],
+    countries: [:ph]
+  }
 end
