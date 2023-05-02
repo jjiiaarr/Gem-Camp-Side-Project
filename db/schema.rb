ae6d0bd6c49fc9984e9fd56a38f45949dd984941
jjiiaarr <jiarr.glorioso@gmail.com>
2023-05-02 14:08:16 +0800

    [Feature 7] generate migration for parent_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_150819) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_022400) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_150819) do
     t.string "total_deposit"
     t.integer "children_members"
     t.string "image"
+    t.integer "parent_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
