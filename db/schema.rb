c82e2dabcd2469604de2bda33f1c498a566ace36
jjiiaarr <jiarr.glorioso@gmail.com>
2023-05-04 18:14:44 +0800

    [Feature 6] add user id to user address and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_022400) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_052431) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_022400) do
     t.bigint "address_barangay_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["address_barangay_id"], name: "index_user_addresses_on_address_barangay_id"
     t.index ["address_city_id"], name: "index_user_addresses_on_address_city_id"
     t.index ["address_province_id"], name: "index_user_addresses_on_address_province_id"
