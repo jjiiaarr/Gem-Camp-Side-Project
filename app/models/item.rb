80eb61f84d6d111292b7a5911d98378de2411596
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-05-05 14:57:31 +0800

    [Feature 11] Done Item Soft Kill

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  default_scope { where(deleted_at: nil) }
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
