614df05e2faecf11b2ad2d90db90d104bfa1b895
jjiiaarr <jiarr.glorioso@gmail.com>
2023-05-05 11:10:20 +0800

    [Feature 6] add address limit to user

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -1,7 +1,10 @@
 class UserAddress < ApplicationRecord
+
+  MAX_ADDRESS = 5
+
   enum genre: { home: 0, office: 1 }
 
-  validates :phone_number, phone: {
+  validates :phone, phone: {
     possible: true,
     allow_blank: true,
     types: %i[voip mobile],
@@ -12,4 +15,15 @@ class UserAddress < ApplicationRecord
   belongs_to :province, class_name: 'Address::Province', foreign_key: 'address_province_id'
   belongs_to :city, class_name: 'Address::City', foreign_key: 'address_city_id'
   belongs_to :barangay, class_name: 'Address::Barangay', foreign_key: 'address_barangay_id'
+
+  validate :number_of_addresses, on: :create
+
+  private
+
+  def number_of_addresses
+
+    if user.user_address.count >= MAX_ADDRESS
+      errors.add(:base,"You have reached the maximum number of addresses.")
+    end
+  end
 end
