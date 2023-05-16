86ae88e99144b0287cc5741ded7755a2a79c1adc
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-05-16 15:56:06 +0800

    [Feature 20] add scope for filter date, coin validation, increase deposit when pay state and fix code in serial_number

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,12 @@
 class Order < ApplicationRecord
   include AASM
 
+  scope :filter_by_date_range, -> (date_range) { where(items: { created_at: date_range }) }
+
+  validates :coin, presence: true, numericality: { greater_than: 0 }
   belongs_to :user
   belongs_to :offer
+  after_create :generate_serial_number
 
   enum genre: { deposit: 0, increase: 1, deduct: 2, bonus: 3, share: 4 }
 
@@ -19,7 +23,7 @@ class Order < ApplicationRecord
     end
 
     event :pay do
-      transitions from: :submitted, to: :paid, success: :pay_coins
+      transitions from: :submitted, to: :paid, success: [:pay_coins, :increase_deposit]
     end
   end
 
@@ -43,13 +47,17 @@ class Order < ApplicationRecord
     user.coins >= coin
   end
 
+  def increase_deposit
+    user.update(total_deposit: user.total_deposit + amount) if genre == 'deposit'
+  end
+
   def decrease_deposit
     user.update(total_deposit: user.total_deposit - amount) if genre == 'deposit'
   end
 
   def generate_serial_number
     number_count = Order.where(user_id: user_id).count.to_s.rjust(4, '0')
-    serial_number = "#{Time.current.strftime("%y%m%d")}-#{order.id}-#{user_id}-#{number_count}"
+    serial_number = "#{Time.current.strftime("%y%m%d")}-#{id}-#{user_id}-#{number_count}"
     self.update(serial_number: serial_number)
   end
 end
