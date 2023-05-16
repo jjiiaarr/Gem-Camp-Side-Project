fb724d34871ec3cbe59dbdc2ea5056678a8cf138
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-05-17 01:18:47 +0800

    [Feature 23] finish client profile page order, lottery, winning and invitation history

--- app/controllers/client/profiles_controller.rb
+++ app/controllers/client/profiles_controller.rb
@@ -1,8 +1,14 @@
 class Client::ProfilesController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: :edit
 
-  def show; end
+  def show
+    @user = current_user
+    @order_history = Order.where(user: @user).limit(5).order(created_at: :desc) if params[:order] == 'order'
+    @lottery_history = @user.bets.limit(5).order(created_at: :desc) if params[:lottery] == 'lottery'
+    @winner_history = Winner.where(user: @user).order(created_at: :desc) if params[:winner] == 'winner'
+    @invitation_history = User.where(parent: @user).order(created_at: :desc) if params[:invitation] == 'invitation'
+  end
 
   def edit; end
 
